14b64d1a30271e4d96fba96477a5b3fef83816a1
BruceChaun <BruceChaun@github.com>
2016-11-06 18:16:24 -0500

    add manhattan distance

--- src/game/BoardPosition.java
+++ src/game/BoardPosition.java
@@ -1,20 +1,27 @@
 package game;
 
 public class BoardPosition {
-	private int row;
-	private int col;
-	
-	public BoardPosition(int row, int col) {
-		this.row = row;
-		this.col = col;
-	}
-	
-	public int Row() {return this.row;}
-	public int Col() {return this.col;}
-	
-	public boolean equals(BoardPosition that) {
-		if (this.row == that.row && this.col == that.col)
-			return true;
-		return false;
-	}
+    private int row;
+    private int col;
+
+    public BoardPosition(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public int Row() {return this.row;}
+    public int Col() {return this.col;}
+
+    public boolean equals(BoardPosition that) {
+        if (this.row == that.row && this.col == that.col)
+            return true;
+        return false;
+    }
+
+    /*
+     * calculate Manhattan distance from this to that
+     */
+    public BoardPosition distance(BoardPosition that) {
+        return new BoardPosition(that.Row() - this.Row(), that.Col() - this.Col());
+    }
 }
